f080587b24181f4751fe80fcb06f9f6bde81625a
wnicolass <nicolaswgl99@gmail.com>
2022-12-31 14:42:09 +0000

    :sparkles: using arrays and objects

--- 008_js_intro/app.js
+++ 008_js_intro/app.js
@@ -1,4 +1,10 @@
-let greetingText = "Helloooooo";
-let age = 32;
-alert("Hi I'm Nic :D");
-alert(age);
+let age = 23;
+let userName = "Nic";
+let hobbies = ["Sports", "Cooking", "Reading"];
+let job = {
+  title: "Developer",
+  place: "New York",
+  salary: 50000,
+};
+
+alert(hobbies[0]);
