2e8a4ed5df04830d2a7b070236ae5d012aa5ac9d
wnicolass <nicolaswgl99@gmail.com>
2023-01-02 12:26:46 +0000

    math operations and strings

---
+++ 008_js_intro/operations.js
@@ -0,0 +1,23 @@
+// console.log(10 + 3);
+// console.log(10 - 3);
+// console.log(10 * 3);
+// console.log(10 / 3);
+// console.log(10 % 3);
+// console.log((10 + 3 - 5.1) * 10);
+
+// let result = (10 + 3 - 5.1) * 10;
+// result = 10 * 4;
+// result++; // result = result + 1;
+// result--; // result = result - 1;
+
+// result += 5; // result = result + 5;
+// result -= 5; // result = result - 5;
+// result /= 5; // result = result / 5;
+// result *= 5; // result = result * 5;
+
+// console.log(result);
+
+console.log("Hello" + " " + "World");
+let username = "Nic";
+console.log(username.length);
+console.log(username.toUpperCase());
