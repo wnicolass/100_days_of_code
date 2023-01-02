ac4d3e0cb2a1b2ca880a91649c42713e0b6b10b8
wnicolass <nicolaswgl99@gmail.com>
2023-01-02 09:42:18 +0000

    create first method

--- 008_js_intro/app.js
+++ 008_js_intro/app.js
@@ -1,25 +1,34 @@
-let courseName = "Web Development Bootcamp";
-let price = 9.99;
-let objectives = [
-  "Get hired",
-  "Learn more about JS",
-  "Build an e-commerce project",
-];
+// let courseName = "Web Development Bootcamp";
+// let price = 9.99;
+// let objectives = [
+//   "Get hired",
+//   "Learn more about JS",
+//   "Build an e-commerce project",
+// ];
 
-let onlineCourse = {
-  name: courseName,
-  price: price,
-  objectives: objectives,
+// let onlineCourse = {
+//   name: courseName,
+//   price: price,
+//   objectives: objectives,
+// };
+
+// alert(onlineCourse.name);
+// alert(onlineCourse.price);
+// alert(onlineCourse.objectives);
+// alert(onlineCourse.objectives[1]);
+
+// function showGoal(listOfGoals, goal) {
+//   return listOfGoals[goal];
+// }
+
+// let myGoal = showGoal(objectives, 2);
+// alert(myGoal);
+
+let person = {
+  name: "Nic",
+  greet() {
+    alert("Hello");
+  },
 };
 
-alert(onlineCourse.name);
-alert(onlineCourse.price);
-alert(onlineCourse.objectives);
-alert(onlineCourse.objectives[1]);
-
-function showGoal(listOfGoals, goal) {
-  return listOfGoals[goal];
-}
-
-let myGoal = showGoal(objectives, 2);
-alert(myGoal);
+person.greet();
