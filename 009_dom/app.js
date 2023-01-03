738a46b30f9d0982e50cac56043c91206b5d88a5
wnicolass <nicolaswgl99@gmail.com>
2023-01-03 11:06:59 +0000

    drilling into the document to change an anchor link

--- 009_dom/app.js
+++ 009_dom/app.js
@@ -1,2 +1,4 @@
 //the main difference between console.log and console.dir is that the .log displays the object in its string representation and .dir recognizes the object as an object and outputs its properties
-console.dir(document);
+// console.dir(document);
+
+document.body.children[1].children[0].href = "https://google.com";
