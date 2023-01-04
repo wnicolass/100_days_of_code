a01bacce21dffbbc8ff39253199349d997e3555a
wnicolass <nicolaswgl99@gmail.com>
2023-01-04 15:11:14 +0000

    get anchor element by id

--- 009_dom/app.js
+++ 009_dom/app.js
@@ -1,4 +1,7 @@
 //the main difference between console.log and console.dir is that the .log displays the object in its string representation and .dir recognizes the object as an object and outputs its properties
 // console.dir(document);
 
-document.body.children[1].children[0].href = "https://google.com";
+// document.body.children[1].children[0].href = "https://google.com";
+
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = "https://google.com";
