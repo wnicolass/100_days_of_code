9653fbbf1c4db9c582d7263514173134ff29039d
wnicolass <nicolaswgl99@gmail.com>
2023-01-08 15:24:25 +0000

    :alembic: while loop

--- 010_control_structures/scripts.js
+++ 010_control_structures/scripts.js
@@ -38,3 +38,11 @@ const loggedUser = {
 // }
 
 document.body.innerText = loggedUser.address?.zip?.city ?? "not informed";
+
+let isFinished = false;
+
+while (!isFinished) {
+  isFinished = confirm("Do you want to quit?");
+}
+
+console.log("Done!");
