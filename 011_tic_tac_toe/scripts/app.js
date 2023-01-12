674e1aa98826d20ad941baad9c8687f50e20b684
wnicolass <nicolaswgl99@gmail.com>
2023-01-12 21:56:04 +0000

    feat: clear player config input and show error message

--- 011_tic_tac_toe/scripts/app.js
+++ 011_tic_tac_toe/scripts/app.js
@@ -1,14 +1,11 @@
-import {
-  openPlayerConfig,
-  closePlayerConfig,
-  backdropElement,
-} from "./config.js";
+import * as Config from "./config.js";
 
 const editPlayerBtns = document.querySelectorAll(".edit-player-btn");
 const cancelConfigurationBtn = document.getElementById("cancel-config-btn");
 
-cancelConfigurationBtn.addEventListener("click", closePlayerConfig);
-backdropElement.addEventListener("click", closePlayerConfig);
+Config.formElement.addEventListener("submit", Config.savePlayerConfig);
+cancelConfigurationBtn.addEventListener("click", Config.closePlayerConfig);
+Config.backdropElement.addEventListener("click", Config.closePlayerConfig);
 editPlayerBtns.forEach((playerBtn) => {
-  playerBtn.addEventListener("click", openPlayerConfig);
+  playerBtn.addEventListener("click", Config.openPlayerConfig);
 });
--- 011_tic_tac_toe/scripts/config.js
+++ 011_tic_tac_toe/scripts/config.js
@@ -1,12 +1,28 @@
 export const backdropElement = document.getElementById("backdrop");
 const playerConfigOverlay = document.getElementById("config-overlay");
+const errorsOutputElement = document.getElementById("config-errors");
+export const formElement = document.querySelector("form");
 
 export const openPlayerConfig = () => {
   playerConfigOverlay.style.display = "block";
   backdropElement.style.display = "block";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
 };
 
 export const closePlayerConfig = () => {
   playerConfigOverlay.style.display = "none";
   backdropElement.style.display = "none";
+};
+
+export const savePlayerConfig = (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get("playername").trim();
+
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name!";
+    return;
+  }
 };
