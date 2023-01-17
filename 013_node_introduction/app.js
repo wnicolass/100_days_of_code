7ff3dff15049cfe3bbbffd1f2502196aa62ab691
wnicolass <nicolaswgl99@gmail.com>
2023-01-17 19:46:41 +0000

    feat: handle one more route

--- 013_node_introduction/app.js
+++ 013_node_introduction/app.js
@@ -1,13 +1,18 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
+  if (req.url === "/currentime") {
+    res
+      .writeHead(200, { "Content-Type": "text/html" })
+      .end(`<h1>${new Date().toISOString()}</h1>`);
+  } else if (req.url === "/") {
+    res
+      .writeHead(200, { "Content-Type": "text/html" })
+      .end("<h1>Hello World!</h1>");
+  }
 
-  res.end(
-    JSON.stringify({
-      data: "Hello World!",
-    })
-  );
+  //   res.statusCode = 200;
+  //   res.end("<h1>Hello World!</h1>");
 });
 
 server.listen(3000);
