0cad94725d7bc70e1e6fd965d08e0a8b72bc9603
wnicolass <nicolaswgl99@gmail.com>
2023-01-18 21:40:49 +0000

    feat: parse user data sent by post request

--- 013_node_introduction/app.js
+++ 013_node_introduction/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 app.listen(3000, () => console.log("App listening on port 3000"));
 
--- 013_node_introduction/routes.js
+++ 013_node_introduction/routes.js
@@ -6,7 +6,15 @@ routes.get("/currenttime", (req, res) =>
 ); //localhost:3000/currenttime
 
 routes.get("/", (req, res) => {
-  res.send("<h1>Hello world!</h1>");
+  res.send(
+    '<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"/><button>Submit</button></form>'
+  );
 }); //localhost:3000/
+
+routes.post("/store-user", (req, res) => {
+  const username = req.body.username;
+  console.log(req.body);
+  res.send("<h1>Username stored!</h1>");
+});
 
 module.exports = routes;
