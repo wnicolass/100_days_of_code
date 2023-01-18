61ab7bdbf630ace4b0759f20258ed7ef7aa9fd69
wnicolass <nicolaswgl99@gmail.com>
2023-01-18 20:05:58 +0000

    feat: use express instead of built-in http node module

--- 013_node_introduction/app.js
+++ 013_node_introduction/app.js
@@ -1,18 +1,23 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/currentime") {
-    res
-      .writeHead(200, { "Content-Type": "text/html" })
-      .end(`<h1>${new Date().toISOString()}</h1>`);
-  } else if (req.url === "/") {
-    res
-      .writeHead(200, { "Content-Type": "text/html" })
-      .end("<h1>Hello World!</h1>");
-  }
+app.use(routes);
+app.listen(3000, () => console.log("App listening on port 3000"));
 
-  //   res.statusCode = 200;
-  //   res.end("<h1>Hello World!</h1>");
-});
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/currentime") {
+//     res
+//       .writeHead(200, { "Content-Type": "text/html" })
+//       .end(`<h1>${new Date().toISOString()}</h1>`);
+//   } else if (req.url === "/") {
+//     res
+//       .writeHead(200, { "Content-Type": "text/html" })
+//       .end("<h1>Hello World!</h1>");
+//   }
 
-server.listen(3000);
+//   //   res.statusCode = 200;
+//   //   res.end("<h1>Hello World!</h1>");
+// });
+
+// server.listen(3000);
