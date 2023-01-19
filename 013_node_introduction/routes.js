d5eb818f76d44603cc0a3a223a5d26005caaec29
wnicolass <nicolaswgl99@gmail.com>
2023-01-19 10:53:01 +0000

    feat: add new users and see users list

--- 013_node_introduction/routes.js
+++ 013_node_introduction/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const routes = express.Router();
+const filePath = path.resolve(__dirname, "data", "users.json");
 
 routes.get("/currenttime", (req, res) =>
   res.send(`<h1>${new Date().toISOString()}</h1>`)
@@ -15,13 +16,29 @@ routes.get("/", (req, res) => {
 
 routes.post("/store-user", (req, res) => {
   const username = req.body.username;
-  const filePath = path.resolve(__dirname, "data", "users.json");
   const fileData = fs.readFileSync(filePath);
   const existingUsers = JSON.parse(fileData);
   existingUsers.push(username);
 
   fs.writeFileSync(filePath, JSON.stringify(existingUsers));
-  res.send("<h1>Username stored!</h1>");
+  res.send(
+    "<h1>User successfully added!</h1><a href='/users'>Click here to see your list of users!</a>"
+  );
+});
+
+routes.get("/users", (req, res) => {
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+
+  let response = "<ul>";
+  existingUsers.forEach((user) => {
+    response += `<li>${user}</li>`;
+  });
+  response += "</ul>";
+
+  res.send(
+    `<h1>Users List below!</h1>${response}<a href="/">Back to form!</a>`
+  );
 });
 
 module.exports = routes;
