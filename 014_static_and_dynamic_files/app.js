1f3a2dc2fca242e84c0c2d77ea5122f4be4f94c6
wnicolass <nicolaswgl99@gmail.com>
2023-01-20 15:23:53 +0000

    feat: add path to public folder with static files

--- 014_static_and_dynamic_files/app.js
+++ 014_static_and_dynamic_files/app.js
@@ -4,7 +4,7 @@ const routes = require("./routes");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.resolve(__dirname, "frontend")));
+app.use(express.static(path.resolve(__dirname, "public")));
 
 app.set("views", path.resolve(__dirname, "src", "views"));
 app.set("view engine", "ejs");
