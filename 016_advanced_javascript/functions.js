53b29989476b1bb303ff9b901a0b6a2afe54995e
wnicolass <nicolaswgl99@gmail.com>
2023-01-25 18:22:36 +0000

    feat: rest parameters and spread operator

--- 016_advanced_javascript/functions.js
+++ 016_advanced_javascript/functions.js
@@ -10,3 +10,19 @@ function greetUser(person, username = "User") {
 
 // greetUser(person);
 // greetUser(person, "Nicolas");
+
+//rest parameters
+function sumUp(...numbers) {
+  let result = 0;
+
+  for (let number of numbers) {
+    result += number;
+  }
+
+  return result;
+}
+
+const inputNumbers = [1, 2, 3, 4, 5, 6];
+
+//spread
+console.log(sumUp(...inputNumbers));
