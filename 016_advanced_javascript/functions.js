78b678ac6e5ca138ac84761058f8faa81c6ddc74
wnicolass <nicolaswgl99@gmail.com>
2023-01-25 11:05:55 +0000

    default parameters values

---
+++ 016_advanced_javascript/functions.js
@@ -0,0 +1,7 @@
+//default parameter value
+function greetUser(greetingPrefix, username = "User") {
+  console.log(`${greetingPrefix} ${username}`);
+}
+
+// greetUser("Hi");
+// greetUser("Hello", "Nicolas");
