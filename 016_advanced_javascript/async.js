5ff41e65ba9e73d97bf99b7d5e268162ca43aad0
wnicolass <nicolaswgl99@gmail.com>
2023-01-26 15:11:22 +0000

    async-await

--- 016_advanced_javascript/async.js
+++ 016_advanced_javascript/async.js
@@ -1,11 +1,30 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 
-function readFile() {
-  let fileData = fs.readFile("data.txt", (err, data) => {
-    if (err) throw err;
+async function readFile() {
+  //calbacks
+  //   let fileData = fs.readFile("data.txt", (err, data) => {
+  //     if (err) throw err;
+  //     console.log("File parsing done!");
+  //     console.log(data.toString());
+  //   });
+
+  //promises
+  //   fs.readFile("data.txt", { encoding: "utf8" })
+  //     .then((data) => {
+  //       console.log("File parsing done!");
+  //       console.log(data);
+  //     })
+  //     .catch((error) => console.log(error));
+
+  //async-await
+  try {
+    const fileData = await fs.readFile("data.txt", { encoding: "utf8" });
+
     console.log("File parsing done!");
-    console.log(data.toString());
-  });
+    console.log(fileData);
+  } catch (error) {
+    console.error(error);
+  }
   console.log("hi there");
 }
 
