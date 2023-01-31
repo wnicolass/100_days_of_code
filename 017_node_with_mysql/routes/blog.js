c6f9b7de8a063d694deea305309091ac3ca863d2
wnicolass <nicolaswgl99@gmail.com>
2023-01-31 21:10:50 +0000

    feat: create connection with database

--- 017_node_with_mysql/routes/blog.js
+++ 017_node_with_mysql/routes/blog.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const db = require("../database/database");
 const router = Router();
 
 router.get("/", (req, res) => {
@@ -9,7 +10,8 @@ router.get("/posts", (req, res) => {
   res.render("posts-list");
 });
 
-router.get("/new-post", (req, res) => {
+router.get("/new-post", async (req, res) => {
+  const authors = await db.query();
   res.render("create-post");
 });
 
