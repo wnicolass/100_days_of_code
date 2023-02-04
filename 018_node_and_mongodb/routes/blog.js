cfb066b06a02c22e6d6fea4dc8c14e472d9217fb
wnicolass <nicolaswgl99@gmail.com>
2023-02-04 17:51:11 +0000

    feat: inserting new data on db

--- 018_node_and_mongodb/routes/blog.js
+++ 018_node_and_mongodb/routes/blog.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const ObjectId = require("mongodb").ObjectId;
 const db = require("../database/config");
 
 const router = express.Router();
@@ -14,6 +15,32 @@ router.get("/posts", function (req, res) {
 router.get("/new-post", async function (req, res) {
   const authors = await db.getDb().collection("authors").find().toArray();
   res.render("create-post", { authors });
+});
+
+router.post("/posts", async (req, res) => {
+  const { title, summary, content } = req.body;
+  const authorId = new ObjectId(req.body.author);
+  const author = await db
+    .getDb()
+    .collection("authors")
+    .findOne({ _id: authorId });
+  const newPost = {
+    title,
+    summary,
+    content,
+    createdAt: new Date(),
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.email,
+    },
+  };
+
+  const result = await db.getDb().collection("posts").insertOne(newPost);
+  console.log(result);
+  if (result) {
+    return res.redirect("posts");
+  }
 });
 
 module.exports = router;
