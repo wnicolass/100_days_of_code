56ac547e429a775f74a566995b31f6236a71bef9
wnicolass <nicolaswgl99@gmail.com>
2023-02-06 20:36:35 +0000

    feat: using multer to get access to files

--- 019_file_uploading/routes/users.js
+++ 019_file_uploading/routes/users.js
@@ -1,13 +1,32 @@
-const express = require('express');
-
+const express = require("express");
+const multer = require("multer");
 const router = express.Router();
+const storageConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+const upload = multer({ storage: storageConfig });
 
-router.get('/', function(req, res) {
-  res.render('profiles');
+router.get("/", function (req, res) {
+  res.render("profiles");
 });
 
-router.get('/new-user', function(req, res) {
-  res.render('new-user');
+router.get("/new-user", function (req, res) {
+  res.render("new-user");
+});
+
+router.post("/profiles", upload.single("image"), (req, res) => {
+  const uploadedImageFile = req.file;
+  const userData = req.body;
+
+  console.log(uploadedImageFile);
+  console.log(userData);
+
+  res.redirect("/");
 });
 
 module.exports = router;
