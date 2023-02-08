64f948ea329e9240374c572a983db1ea59b4ec65
wnicolass <nicolaswgl99@gmail.com>
2023-02-08 21:01:26 +0000

    feat: display comments

--- 020_ajax/public/scripts/comments.js
+++ 020_ajax/public/scripts/comments.js
@@ -1,13 +1,33 @@
 const loadCommentsBtn = document.getElementById("load-comments-btn");
 const commentsSection = document.getElementById("comments");
 
+function createCommentsList(comments) {
+  const commentListEl = document.createElement("ol");
+
+  for (const comment of comments) {
+    const commentEl = document.createElement("li");
+    commentEl.innerHTML = `
+        <article class="comment-item">
+            <h2>${comment.title}</h2>
+            <p>${comment.text}</p>
+        </article>
+        `;
+    commentListEl.appendChild(commentEl);
+  }
+
+  return commentListEl;
+}
+
 async function fetchCommentsForPost() {
   const postId = loadCommentsBtn.dataset.postid;
 
   try {
     const res = await fetch(`/posts/${postId}/comments`);
     const data = await res.json();
-    console.log(data);
+
+    const commentsListEl = createCommentsList(data);
+    commentsSection.innerHTML = "";
+    commentsSection.appendChild(commentsListEl);
   } catch (err) {
     console.error(err.message);
   }
