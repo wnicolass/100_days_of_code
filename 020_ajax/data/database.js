74847410b21a65bcbfd9ff9c143d8a8eced4ab18
wnicolass <nicolaswgl99@gmail.com>
2023-02-08 20:26:28 +0000

    feat: add comments.js file

--- 020_ajax/data/database.js
+++ 020_ajax/data/database.js
@@ -1,22 +1,22 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
 let database;
 
 async function connect() {
-  const client = await MongoClient.connect('mongodb://localhost:27017');
-  database = client.db('blog');
+  const client = await MongoClient.connect("mongodb://localhost:27017");
+  database = client.db("blogj");
 }
 
 function getDb() {
   if (!database) {
-    throw { message: 'Database connection not established!' };
+    throw { message: "Database connection not established!" };
   }
   return database;
 }
 
 module.exports = {
   connectToDatabase: connect,
-  getDb: getDb
+  getDb: getDb,
 };
