da53a779a35f11a558fc2473bff2cf0a183a0418
wnicolass <nicolaswgl99@gmail.com>
2023-02-08 21:30:15 +0000

    feat: sending a json response

--- 020_ajax/routes/blog.js
+++ 020_ajax/routes/blog.js
@@ -133,7 +133,7 @@ router.post("/posts/:id/comments", async function (req, res) {
     text: req.body.text,
   };
   await db.getDb().collection("comments").insertOne(newComment);
-  res.redirect("/posts/" + req.params.id);
+  res.json({ message: "ok" });
 });
 
 module.exports = router;
