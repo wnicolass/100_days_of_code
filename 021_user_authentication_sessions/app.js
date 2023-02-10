fb199fa4a6743e47b0f4f963b09b679a9c919a17
wnicolass <nicolaswgl99@gmail.com>
2023-02-10 13:32:31 +0000

    feat: configure session authentication

--- 021_user_authentication_sessions/app.js
+++ 021_user_authentication_sessions/app.js
@@ -1,23 +1,40 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
+const session = require("express-session");
+const mongodbStore = require("connect-mongodb-session");
 
-const db = require('./data/database');
-const demoRoutes = require('./routes/demo');
+const db = require("./data/database");
+const demoRoutes = require("./routes/demo");
 
+const MongoStore = mongodbStore(session);
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+const sessionStore = new MongoStore({
+  uri: "localhost:27017",
+  databaseName: "auth-demo",
+  collection: "sessions",
+});
 
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "super-secret",
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+  })
+);
 
 app.use(demoRoutes);
 
-app.use(function(error, req, res, next) {
-  res.render('500');
-})
+app.use(function (error, req, res, next) {
+  res.render("500");
+});
 
 db.connectToDatabase().then(function () {
   app.listen(3000);
