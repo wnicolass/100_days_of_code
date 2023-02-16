e4f89f42c465375305623c27480fa38142f3b643
wnicolass <nicolaswgl99@gmail.com>
2023-02-16 00:05:17 +0000

    feat: end sql injection studies

--- 022_website_security/db-injection/data/database.js
+++ 022_website_security/db-injection/data/database.js
@@ -1,11 +1,10 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  database: 'security',
-  user: 'root',
-  password: 'your-pw',
-  multipleStatements: true
-})
+  host: "localhost",
+  database: "security",
+  user: "root",
+  password: "",
+});
 
 module.exports = pool;
