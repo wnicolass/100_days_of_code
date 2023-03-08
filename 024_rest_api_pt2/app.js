475fe234493721553c7d5558c01080c1fff96186
wnicolass <nicolaswgl99@gmail.com>
2023-03-08 21:48:39 +0000

    feat: add middleware to allow cors

--- 024_rest_api_pt2/app.js
+++ 024_rest_api_pt2/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const db = require("./data/database");
 const todosRouter = require("./routes/todos.routes");
+const enableCors = require("./middlewares/cors");
 const app = express();
 
+app.use(enableCors);
 app.use(express.json());
 app.use("/todos", todosRouter);
 app.use(function (error, req, res, next) {
---
+++ 024_rest_api_pt2/middlewares/cors.js
@@ -0,0 +1,14 @@
+function enableCors(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", [
+    "GET",
+    "POST",
+    "PATCH",
+    "DELETE",
+    "OPTIONS",
+  ]);
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+}
+
+module.exports = enableCors;
