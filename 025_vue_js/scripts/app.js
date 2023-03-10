e8032645ee267587a65ec8b92e743db9aad07f0f
wnicolass <nicolaswgl99@gmail.com>
2023-03-10 12:16:58 +0000

    feat: update existing todo

--- 025_vue_js/scripts/app.js
+++ 025_vue_js/scripts/app.js
@@ -5,16 +5,42 @@ const TodosApp = {
     return {
       todos: [],
       enteredTodoText: "",
+      editedTodoId: null,
     };
   },
   methods: {
     saveTodo(event) {
       event.preventDefault();
+      if (this.editedTodoId) {
+        this.updateTodo();
+        return;
+      }
       const newTodo = {
         text: this.enteredTodoText,
         id: new Date().toISOString(),
       };
       this.todos.push(newTodo);
+      this.enteredTodoText = "";
+    },
+    startEditTodo(todoId) {
+      this.editedTodoId = todoId;
+      const todo = this.todos.find((item) => item.id === todoId);
+      this.enteredTodoText = todo.text;
+    },
+    updateTodo() {
+      if (!this.editedTodoId) {
+        return;
+      }
+      const todoIndex = this.todos.findIndex(
+        (item) => item.id === this.editedTodoId
+      );
+      const updatedTodoItem = {
+        id: this.todos[todoIndex].id,
+        text: this.enteredTodoText,
+      };
+
+      this.todos[todoIndex] = updatedTodoItem;
+      this.editedTodoId = null;
       this.enteredTodoText = "";
     },
   },
