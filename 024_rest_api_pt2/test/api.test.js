c3b367e510acbcf8f97dd76685ad96f4d76bdbd0
wnicolass <nicolaswgl99@gmail.com>
2023-05-14 22:18:14 +0100

    feat: create first tests with the native test runner

--- 024_rest_api_pt2/test/api.test.js
+++ 024_rest_api_pt2/test/api.test.js
@@ -9,14 +9,30 @@ describe('API Workflow', () => {
     before(async () => {
         server = app.listen(3001);
         await new Promise(resolve => server.once('listening', resolve));
-    })
+    });
+
+    it('should create a todo and return a success message', async () => {
+        const text = 'I am a todo from test';
+        const response = await fetch(`${BASE_URL}/todos`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                text
+            }),
+        });
+        strictEqual(response.status, 201);
+        const responseData = await response.json();
+        console.log(responseData);
+    });
 
     it('should return a list of todo objects', async () => {
         const response = await fetch(`${BASE_URL}/todos`);
         strictEqual(response.status, 200);
         const responseData = await response.json();
         ok(responseData.todos instanceof Array, 'todos should be an array');
-    })
+    });
 
     after(done => server.close(done));
 })
